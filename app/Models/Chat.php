e5c339f74ff16efee1fac1114161c3a6ab1aa2ee
Wahid Ahmad <wahid.a@kodextech.com>
2023-06-27 16:27:28 +0500

    Update Chat.php

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -37,7 +37,7 @@ class Chat extends Model
 
     public function lastMessage()
     {
-        return $this->hasMany(Message::class)->latest('created_at')->limit(1)->first();
+        return $this->hasMany(Message::class)->latest('created_at')->first();
     }
 
     public function LastMessageTimeDifference()
