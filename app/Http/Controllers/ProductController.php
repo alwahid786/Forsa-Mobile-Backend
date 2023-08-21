b9eb5e1529e0cd9a207212c3106394f42e95ef7e
Wahid Ahmad <wahid.a@kodextech.com>
2023-08-21 14:39:33 +0500

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -183,8 +183,8 @@ class ProductController extends Controller
                 $productsData->where('category_id', $request->sub_category);
             }
             if (!$request->has('sub_category') || $request->sub_category == 0) {
-                if ($request->has('category')) {
-                    $productsData->where('category_id', $request->category);
+                if ($request->has('category_id')) {
+                    $productsData->where('category_id', $request->category_id);
                 }
             }
         }
