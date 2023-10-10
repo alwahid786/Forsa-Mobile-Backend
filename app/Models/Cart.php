462d8abdd8e27a137b3be511d669dabde08f6c5b
mubeen <mubeen.kodex@gmail.com>
2023-10-10 14:09:23 +0500

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -12,9 +12,4 @@ class Cart extends Model
     protected $fillable = [
         'product_id', 'user_id'
     ];
-
-    public function product()
-    {
-        return $this->belongsTo(Product::class, 'product_id');
-    }
 }
