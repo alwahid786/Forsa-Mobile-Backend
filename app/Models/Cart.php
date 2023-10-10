aa62e050701b8409615b82c95b553f318856f3ee
mubeen <mubeen.kodex@gmail.com>
2023-10-10 14:22:46 +0500

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -15,6 +15,6 @@ class Cart extends Model
 
     public function product()
     {
-        return $this->hasOne(Product::class, 'product_id');
+        return $this->belongsTo(Product::class);
     }
 }
